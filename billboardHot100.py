fa46e40905a8f340b9a1461f9bbbacd0c4e7fb1c
Alexander Crawford <mail@alexander-crawford.com>
2021-06-12 14:23:53 +0200

    add status code to json response

--- billboardHot100.py
+++ billboardHot100.py
@@ -1,39 +1,59 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+import re
+from collections import OrderedDict
 
-# set url
-url = "https://www.billboard.com/charts/top-album-sales"
+def billboard():
+    # set url
+    url = "https://www.billboard.com/charts/top-album-sales"
 
-# get the page
-data = requests.get(url)
+    # get the page
+    page = requests.get(url)
 
-# get bs4 object
-soup = BeautifulSoup(data.text,'html.parser')
+    # get bs4 object
+    soup = BeautifulSoup(page.text,'html.parser')
 
-# create list to be returned
-data = []
+    # create dictionary to be returned
+    response = OrderedDict()
 
-# iterate over soup object
-for li in soup.find_all('div','chart-list-item'):
+    # add status, assume success with code 200
+    response['meta'] = {
+        'status' : 200,
+        'status_message' : 'success'
+    }
 
-    # create dictionary
-    dict = {}
+    # create data list
+    data = []
 
-    # add position
-    for position in li.find_all('div','chart-list-item__rank'):
-        dict['position'] = position.get_text().replace('\n','')
+    # iterate over soup object
+    for li in soup.find_all('div','chart-list-item'):
 
-    # add artist
-    for artist in li.find_all('div','chart-list-item__artist'):
-        dict['artist'] = artist.get_text().replace('\n','')
+        # create dictionary
+        dict = {}
 
-    # add album
-    for album in li.find_all('span','chart-list-item__title-text'):
-        dict['album'] = album.get_text().replace('\n','')
+        # add position
+        for position in li.find_all('div','chart-list-item__rank'):
+            dict['position'] = re.sub('[^0-9]','',position.get_text())
 
-    # add dictionary to list
-    data.append(dict)
+        # add artist
+        for artist in li.find_all('div','chart-list-item__artist'):
+            dict['artist'] = re.sub('[^a-zA-Z]','',artist.get_text())
 
-# print output as json
-print(json.dumps(data,sort_keys=True, indent=4))
+        # add album
+        for album in li.find_all('span','chart-list-item__title-text'):
+            dict['album'] = album.get_text().replace('\n','')
+
+        # add dictionary to list
+        data.append(dict)
+
+    # add data list to response dictionary
+    response['data'] = data
+
+    # print output as json
+    print(json.dumps(response,indent=4))
+
+    # return data
+    return response
+
+billboard()
