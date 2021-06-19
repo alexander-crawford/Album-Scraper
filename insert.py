47975832a1af9f1adcd245cc04b61338d73a4476
Alexander Crawford <mail@alexander-crawford.com>
2021-06-19 16:22:24 +0200

    complete first version of functions in insert.py

--- insert.py
+++ insert.py
@@ -13,17 +13,62 @@ if data['meta']['status']==200:
     cursor = cnx.cursor()
 
     add_artist = (
-    "INSERT IGNORE INTO artist"
-    "(name)"
-    "VALUES (%s)"
+        "INSERT INTO artist "
+        "(name) "
+        "VALUES (%s)"
     )
-    list = []
+    get_artist = (
+        "SELECT id FROM artist "
+        "WHERE name = (%s)"
+    )
+    add_album = (
+        "INSERT INTO album "
+        "(title) "
+        "VALUES (%s)"
+    )
+    get_album = (
+        "SELECT album.id FROM album "
+        "INNER JOIN artist_album ON album.id = artist_album.album_id "
+        "INNER JOIN artist ON artist_album.artist_id = artist.id "
+        "WHERE artist.id = %(artist)s AND album.title = %(album)s"
+    )
+    add_artist_album = (
+        "INSERT INTO artist_album "
+        "(artist_id,album_id) "
+        "VALUES (%(artist)s,%(album)s)"
+    )
     for data in data['data']:
-        tuple = (data['artist'],)
-        list.append(tuple)
-    print(list)
+        artist_id = ''
+        album_id = ''
 
-    cursor.executemany(add_artist,list)
+        # check if artist is already in database
+        cursor.execute(get_artist,(data['artist'],))
+        result = cursor.fetchone()
+        if result==None:
+            # if not in database add artist
+            cursor.execute(add_artist,(data['artist'],))
+            # get id of new row
+            artist_id = cursor.lastrowid
+        else:
+            # if in database get artist id
+            artist_id = result[0]
+
+        # check if album is already in database
+        cursor.execute(get_album,{
+            'artist' : artist_id,
+            'album' : data['album']
+        })
+        result = cursor.fetchone()
+        if result==None:
+            # if not in database add album
+            cursor.execute(add_album,(data['album'],))
+            album_id = cursor.lastrowid
+
+            # join album and artist
+            cursor.execute(add_artist_album,{
+                'artist' : artist_id,
+                'album' : album_id
+            })
 
     cnx.commit()
     cursor.close()
