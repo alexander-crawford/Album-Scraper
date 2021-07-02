069777ff9a8dc9cc95491577ec909e85da98b55e
Alexander Crawford <mail@alexander-crawford.com>
2021-07-02 12:56:58 +0200

    alters discogs api call to return both album cover and album year

--- discogs.py
+++ discogs.py
@@ -1,24 +1,28 @@
 import requests
-import keys
+import config
 from ratelimit import limits
 
 if __name__ == '__main__':
     getAlbumCover(seach_string)
 
-# no more than 60 calls in a 1 minute period 
+# no more than 60 calls in a 1 minute period
 @limits(calls=60,period=60)
 def getAlbumCover(seach_string):
     url = "https://api.discogs.com/database/search"
 
     querystring = {
-        "key":keys.discogs_key,
-        "secret":keys.discogs_secret,
+        "key":config.discogs_key,
+        "secret":config.discogs_secret,
         "q":seach_string
     }
 
     response = requests.request("GET", url, params=querystring)
 
+    dict = {}
+
     if response.json()['results'][0]['type']=='master' and response.status_code == 200:
-        return response.json()['results'][0]['cover_image']
+        dict['album_cover_url'] = response.json()['results'][0]['cover_image']
+        dict['album_year'] = response.json()['results'][0]['year']
+        return dict
     else:
         return None
