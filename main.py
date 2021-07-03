8474e00f44b5dd01173c190c75d32519644a8d81
Alexander Crawford <mail@alexander-crawford.com>
2021-07-03 13:29:32 +0200

    refactor scraper

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from scraper_db import connect
+from scrapers import billboard
 
+# create connection to scraper_db
 cnx = connect.connect()
 
-print(cnx)
+# get resuts from scraper
+result = billboard.get()
