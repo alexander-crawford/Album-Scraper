65dc81ace8b2e4cf25b1b606e7e0d067a927ab3d
Alexander Crawford <mail@alexander-crawford.com>
2021-07-15 16:28:58 +0200

    alter main.py to new function names

--- main.py
+++ main.py
@@ -6,15 +6,19 @@ import api
 cnx = scraper_db.connect()
 
 # get results from scraper
-# result = billboard.get()
+result = billboard.get()
 
 # insert into db providing connection and results
-# scraper_db.insert(cnx,result)
+scraper_db.insert(cnx,result)
 
-# get albums with no year and image
-print('no year and image')
-for item in scraper_db.getNoYearAndImage(cnx):
-    print(api.getAlbumInfo(item[0] + " " + item[1]))
+# get albums where image or year is missing 
+for item in scraper_db.getDiscogsAlbums(cnx):
+    result = api.getAlbumInfo(item[1] + " " + item[2])
+    if result!=None:
+        scraper_db.addYear(cnx,result['album_year'],item[0])
+
+    # TODO: download images
+
 
 # disconnect from scraper_db
 cnx.close()
