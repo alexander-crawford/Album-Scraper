7b983ec0819188a16face48c8c0246cb883db43f
Alexander Crawford <mail@alexander-crawford.com>
2021-07-15 16:25:34 +0200

    add function that returns rows with no album art or year to call discogs api with

--- scraper_db/select.py
+++ scraper_db/select.py
@@ -1,61 +1,20 @@
-if __name__ == '__main__':
-    getNoYear()
-    getNoImage()
-
-def getNoYear(cnx):
-    # create cursor
-    cursor = cnx.cursor()
-
-    # create select statement
-    get_no_year = (
-        "SELECT artist.name AS Aritst, "
-        "album.title AS Album "
-        "FROM album "
-        "INNER JOIN artist_album ON album.id = artist_album.album_id "
-        "INNER JOIN artist ON artist_album.artist_id = artist.id "
-        "WHERE album.year IS NULL"
-    )
-
-    # run statement
-    cursor.execute(get_no_year)
-
-    # return result
-    return cursor.fetchall()
-
-def getNoImage(cnx):
+def getDiscogsAlbums(cnx):
     # create cursor
     cursor = cnx.cursor()
 
     # create select statement
     get_no_image = (
-        "SELECT artist.name AS Aritst, "
-        "album.title AS Album "
+        "SELECT album.id, "
+        "album.title, "
+        "artist.name, "
+        "album.year, "
+        "album.image_lrg "
         "FROM album "
         "INNER JOIN artist_album ON album.id = artist_album.album_id "
         "INNER JOIN artist ON artist_album.artist_id = artist.id "
-        "WHERE album.image_lrg IS NULL OR "
-        "album.image_sml IS NULL"
-    )
-
-    # run statement
-    cursor.execute(get_no_image)
-
-    # return result
-    return cursor.fetchall()
-
-def getNoYearAndImage(cnx):
-    # create cursor
-    cursor = cnx.cursor()
-
-    # create select statement
-    get_no_image = (
-        "SELECT artist.name AS Aritst, "
-        "album.title AS Album "
-        "FROM album "
-        "INNER JOIN artist_album ON album.id = artist_album.album_id "
-        "INNER JOIN artist ON artist_album.artist_id = artist.id "
-        "WHERE album.image_lrg IS NULL AND "
-        "album.image_sml IS NULL"
+        "WHERE (album.image_lrg IS NULL OR "
+        "album.year IS NULL) "
+        "AND album.discogs_api = 0"
     )
 
     # run statement
