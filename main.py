16d96ee806858e946260e846aeb9bcc9ecd8c52c
Alexander Crawford <mail@alexander-crawford.com>
2021-07-29 21:08:40 +0200

    add download image function

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ result = billboard.get()
 # insert into db providing connection and results
 scraper_db.insert(cnx,result)
 
-# get albums where image or year is missing 
+# get albums where image or year is missing
 for item in scraper_db.getDiscogsAlbums(cnx):
     result = api.getAlbumInfo(item[1] + " " + item[2])
     if result!=None:
+        # addYear(cnx,year,album_id)
         scraper_db.addYear(cnx,result['album_year'],item[0])
-
-    # TODO: download images
-
+        # addImage(cnx,url,album_id)
+        scraper_db.addImage(cnx,result['album_cover_url'],item[0])
 
 # disconnect from scraper_db
 cnx.close()
--- scraper_db/update.py
+++ scraper_db/update.py
@@ -1,3 +1,8 @@
+import pathlib
+import requests
+import os
+import hashlib
+
 def addYear(cnx,year,album_id):
 
     add_year = (
@@ -15,3 +20,52 @@ def addYear(cnx,year,album_id):
 
     cnx.commit()
     cursor.close()
+
+def addImage(cnx,url,album_id):
+    # set temp path
+    temp_path = "../img/tmp/"
+    # create temp directory
+    pathlib.Path(temp_path).mkdir(parents=True, exist_ok=True)
+    # create temp file name
+    temp_filename = url.split("/")[-1]
+
+    # download image to temp location
+    r = requests.get(url, stream = True)
+    if r.status_code == 200:
+        with open(temp_path + temp_filename,"wb") as file:
+            for chunk in r:
+                 if chunk:
+                     file.write(chunk)
+
+        # hash primary key
+        h = hashlib.new("md5")
+        h.update(str(album_id).encode())
+        hash = h.hexdigest()
+
+        # create new path and filename
+        new_path = hash[0:2] + "/" + hash[3:5] + "/"
+        new_filename = hash[6:]
+        filetype = pathlib.Path(temp_path+temp_filename).suffix
+
+        # create path
+        pathlib.Path(temp_path[0:7]+ new_path).mkdir(parents=True, exist_ok=True)
+
+        # move file
+        os.rename(temp_path + temp_filename,temp_path[0:7] + new_path + new_filename + filetype)
+
+        # insert image location into db
+        add_image = (
+            "UPDATE album "
+            "SET image_lrg = %(image)s "
+            "WHERE id = %(id)s"
+        )
+
+        cursor = cnx.cursor()
+
+        cursor.execute(add_image,{
+            'image' : new_path + new_filename + filetype,
+            'id' : album_id
+        })
+
+        cnx.commit()
+        cursor.close()
