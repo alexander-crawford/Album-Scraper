5d5277977ff9a0e14ab81f71c6faf0b0ec964209
Alexander Crawford <mail@alexander-crawford.com>
2021-07-29 21:08:01 +0200

    move add image from insert script to update script

--- scraper_db/__init__.py
+++ scraper_db/__init__.py
@@ -1,5 +1,5 @@
 from .connect import connect
 from .insert import insert
-from .insert import addImage
 from .select import getDiscogsAlbums
 from .update import addYear
+from .update import addImage
