d96fc43a5ab8d014dfc18e427e59c3c0e3a0c99e
Alexander Crawford <mail@alexander-crawford.com>
2021-07-29 21:05:59 +0200

    add image and year columns to index.php table

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ $result = $mysqli->query("
   SELECT source.title AS source,
   list.position AS position,
   album.title AS album,
+  album.year AS year,
+  album.image_lrg AS image,
   artist.name AS artist FROM list
   INNER JOIN source ON list.source_id = source.id
   INNER JOIN album ON list.album_id = album.id
@@ -24,9 +26,13 @@ $mysqli->close();
     border-collapse: collapse;
     padding: 5px;
   }
+  img{
+    max-width: 100%;
+  }
 </style>
 
 <h1><?php echo $result->fetch_row()[0] ?></h1>
+
 
 <table>
     <tbody>
@@ -34,12 +40,16 @@ $mysqli->close();
             <td>Position</td>
             <td>Artist</td>
             <td>Album</td>
+            <td>Year</td>
+            <td>Image</td>
         </tr>
         <?php foreach ($result as $row): ?>
           <tr>
             <td><?php echo $row['position'] ?></td>
             <td><?php echo $row['artist'] ?></td>
             <td><?php echo $row['album'] ?></td>
+            <td><?php echo $row['year'] ?></td>
+            <td><img src="./img/<?php echo $row['image'] ?>" alt=""></td>
           </tr>
         <?php endforeach; ?>
     </tbody>
