6468ceff7c73808240d46f4230f0c67abc06d88c
Alexander Crawford <mail@alexander-crawford.com>
2021-07-31 15:50:41 +0200

    removes white space from page edges

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 var grid = document.querySelector('.grid');
 var msnry = new Masonry( grid, {
-  itemSelector: '.grid-item'
+  itemSelector: '.grid-item',
+  columnWidth: '.grid-item',
+  percentPosition:true
 });
 imagesLoaded( grid ).on( 'progress', function() {
   msnry.layout();
