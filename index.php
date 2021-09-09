0179d5831dc06663894e0736ed143379268b6a66
Alexander Crawford <mail@alexander-crawford.com>
2021-09-09 16:55:03 +0200

    adds function to return all albums when given an artist id

--- index.php
+++ index.php
@@ -1,25 +1,3 @@
-<?php
-
-$mysqli = new mysqli("127.0.0.1", "root", "123456", "scraper_db",3306);
-
-$result = $mysqli->query("
-  SELECT source.title AS source,
-  artist.id AS artist_id,
-  IFNULL(CONCAT('./img/',album.image_lrg),'./img/blank.svg') AS image,
-  list.position AS position,
-  album.title AS title,
-  artist.name AS artist,
-  album.year AS year FROM list
-  INNER JOIN source ON list.source_id = source.id
-  INNER JOIN album ON list.album_id = album.id
-  INNER JOIN artist_album ON album.id = artist_album.album_id
-  INNER JOIN artist ON artist_album.artist_id = artist.id
-  ORDER BY list.position;
-");
-
-$mysqli->close();
-
-?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -33,6 +11,28 @@ $mysqli->close();
   </head>
   <body>
       <?php if (is_null($_SERVER['QUERY_STRING'])): ?>
+        <?php
+
+        $mysqli = new mysqli("127.0.0.1", "root", "123456", "scraper_db",3306);
+
+        $result = $mysqli->query("
+          SELECT source.title AS source,
+          artist.id AS artist_id,
+          IFNULL(CONCAT('./img/',album.image_lrg),'./img/blank.svg') AS image,
+          list.position AS position,
+          album.title AS title,
+          artist.name AS artist,
+          album.year AS year FROM list
+          INNER JOIN source ON list.source_id = source.id
+          INNER JOIN album ON list.album_id = album.id
+          INNER JOIN artist_album ON album.id = artist_album.album_id
+          INNER JOIN artist ON artist_album.artist_id = artist.id
+          ORDER BY list.position;
+        ");
+
+        $mysqli->close();
+
+        ?>
         <div class="grid">
           <div class="stamp">
             <h1><?php echo ucfirst($result->fetch_row()[0]) ?></h1>
@@ -51,7 +51,43 @@ $mysqli->close();
           <?php endforeach; ?>
         </div>
       <?php else: ?>
-        <?php echo $_SERVER['QUERY_STRING']; ?>
+        <?php
+
+        $mysqli = new mysqli("127.0.0.1", "root", "123456", "scraper_db",3306);
+
+        $statement = $mysqli->prepare("
+          SELECT IFNULL(CONCAT('./img/',album.image_lrg),'./img/blank.svg') AS image,
+          album.title AS title,
+          artist.name AS artist,
+          album.year AS year
+          FROM artist INNER JOIN artist_album ON artist.id = artist_album.artist_id
+          INNER JOIN album ON artist_album.album_id = album.id
+          WHERE artist.id = (?);
+        ");
+
+        $id = $_SERVER['QUERY_STRING'];
+
+        $statement->bind_param("i",$id);
+
+        $statement->execute();
+
+        $result = $statement->get_result();
+
+        $mysqli->close();
+
+        ?>
+
+        <?php foreach ($result as $row): ?>
+          <div class="grid-item" onclick="onClick(this)">
+            <span class="id" hidden></span>
+            <img src="<?php echo $row['image'] ?>" alt="">
+            <div class="text-container">
+              <p class="title"><?php echo $row['title'] ?></p>
+              <p class="artist"><?php echo $row['artist'] ?></p>
+              <p class="year"><?php echo $row['year'] ?></p>
+            </div>
+          </div>
+        <?php endforeach; ?>
       <?php endif; ?>
   </body>
 </html>
