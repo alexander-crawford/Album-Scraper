c635b95a8849deebcbe5596583493b084703fc13
Alexander Crawford <mail@alexander-crawford.com>
2021-09-10 16:17:42 +0200

    Adds initial function to retreive all albums given by artist

--- js/script.js
+++ js/script.js
@@ -3,8 +3,24 @@ var iso = new Isotope( grid, {
   itemSelector: '.grid-item',
   columnWidth: '.grid-item',
   percentPosition:true,
-  stamp: '.stamp'
+  stamp: '.stamp',
+  sortBy:'position',
+  getSortData: {
+    'position': '.position parseInt'
+  }
 });
 imagesLoaded( grid ).on( 'progress', function() {
   iso.layout();
 });
+function onClick(data) {
+  artist_id = data.children[0].innerText;
+  album_position = data.children[3].children[0].innerText;
+  album_id = data.children[1].innerText; 
+  var request = new XMLHttpRequest();
+  request.open('GET','http://localhost:8000/?position=' + album_position + '&artist_id=' + artist_id + '&album_id=' + album_id);
+  request.responseType = "document";
+  request.onload = function () {
+    iso.insert(this.responseXML.body.children)
+  }
+  request.send();
+};
