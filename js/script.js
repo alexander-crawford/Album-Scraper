56545bf7adc3a18405c36c8cb03fa65f81663fb8
Alexander Crawford <mail@alexander-crawford.com>
2021-09-13 15:10:16 +0200

    alter js function to make use of get elements by class name method

--- js/script.js
+++ js/script.js
@@ -13,9 +13,9 @@ imagesLoaded( grid ).on( 'progress', function() {
   iso.layout();
 });
 function onClick(data) {
-  artist_id = data.children[0].innerText;
-  album_position = data.children[3].children[0].innerText;
-  album_id = data.children[1].innerText; 
+  artist_id = data.getElementsByClassName('artist_id')[0].innerText;
+  album_position = data.getElementsByClassName('position')[0].innerText;
+  album_id = data.getElementsByClassName('album_id')[0].innerText;
   var request = new XMLHttpRequest();
   request.open('GET','http://localhost:8000/?position=' + album_position + '&artist_id=' + artist_id + '&album_id=' + album_id);
   request.responseType = "document";
