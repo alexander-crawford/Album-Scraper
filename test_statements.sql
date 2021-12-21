d448cd53b79731f6685044d292448e323014f1fb
Alexander Crawford <mail@alexander-crawford.com>
2021-12-21 22:58:58 +0100

    alter test sql statements, billboard scraper no longer functions, replaced with pitchfork

--- test_statements.sql
+++ test_statements.sql
@@ -1,6 +1,5 @@
 use scraper_db;
-INSERT INTO source (title,url,container_tag,container_class,artist_tag,artist_class,album_tag,album_class) VALUES ('billboard','https://www.billboard.com/charts/top-album-sales','div','chart-list-item','div','chart-list-item__artist','span','chart-list-item__title-text');
 
-select count(source_album.album_id) as Position,artist.name as Artist, album.title as Album, album.year as Year from source_album left join album on source_album.album_id = album.id left join artist on album.artist_id = artist.id group by album.id order by Position,Year desc;
+INSERT INTO source (title,url,container_tag,container_class,artist_tag,artist_class,album_tag,album_class) VALUES ('pitchfork','https://pitchfork.com/reviews/albums','div','review','ul','artist-list review__title-artist','h2','review__title-album');
 
 insert into api (name) values ("discogs");
