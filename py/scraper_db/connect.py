828817ee3681a65e3be6c2871cff5d2269ef5b71
Alexander Crawford <mail@alexander-crawford.com>
2021-12-27 16:56:45 +0100

    create main table when connecting to database

--- py/scraper_db/connect.py
+++ py/scraper_db/connect.py
@@ -87,6 +87,23 @@ def connect():
         ") ENGINE=InnoDB"
     )
 
+    # create main table
+    TABLES['main'] = (
+        "CREATE TABLE IF NOT EXISTS main AS "
+        "SELECT ROW_NUMBER() OVER (ORDER BY COUNT(source_album.album_id) DESC, "
+        "album.year DESC,artist.name ASC, album.title ASC) position, "
+        "artist.id AS artist_id, "
+        "album.id AS album_id, "
+        "IFNULL(CONCAT('./img/',album.image),'./blank.svg') AS image, "
+        "album.title AS title, "
+        "artist.name AS artist, "
+        "album.year AS year "
+        "FROM album "
+        "LEFT JOIN source_album ON album.id = source_album.album_id "
+        "INNER JOIN artist ON album.artist_id = artist.id "
+        "GROUP BY album.id;"
+    )
+
     # create connection to mysql database
     cnx = mysql.connector.connect(user=config.mysql_username,password=config.mysql_password)
 
