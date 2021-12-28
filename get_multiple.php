625b254e130eabad0c9969b76aa40eca63c2f633
Alexander Crawford <mail@alexander-crawford.com>
2021-12-28 12:51:19 +0100

    alter sql statement to make use of new main table

--- get_multiple.php
+++ get_multiple.php
@@ -1,20 +1,12 @@
 <?php
 
   $statement = $mysqli->prepare("
-    SELECT ROW_NUMBER() OVER (ORDER BY count(source_album.album_id),
-    album.year DESC,artist.name ASC, album.title ASC) position,
-    artist.id AS artist_id,
-    album.id AS album_id,
-    IFNULL(CONCAT('./img/',album.image),'./blank.svg') AS image,
-    album.title AS title,
-    artist.name AS artist,
-    album.year AS year
-    FROM album
-    LEFT JOIN source_album ON album.id = source_album.album_id
-    INNER JOIN artist ON album.artist_id = artist.id
-    GROUP BY album.id
+
+    SELECT * FROM main
+    ORDER BY position
     LIMIT 8
     OFFSET ?;
+
   ");
 
   $page = 0;
