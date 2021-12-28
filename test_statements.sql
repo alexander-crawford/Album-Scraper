0d2788a6d6da33b395206a2cec88dbaff2ccc693
Alexander Crawford <mail@alexander-crawford.com>
2021-12-28 13:09:55 +0100

    add test statements for new sources

--- test_statements.sql
+++ test_statements.sql
@@ -1,5 +1,15 @@
 use scraper_db;
 
 INSERT INTO source (title,url,container_tag,container_class,artist_tag,artist_class,album_tag,album_class) VALUES ('pitchfork','https://pitchfork.com/reviews/albums','div','review','ul','artist-list review__title-artist','h2','review__title-album');
+INSERT INTO source (title,url,container_tag,container_class,artist_tag,artist_class,album_tag,album_class) VALUES ('pitchfork','https://pitchfork.com/reviews/albums','div','review','ul','artist-list review__title-artist','h2','review__title-album');
+INSERT INTO source (title,url,container_tag,container_class,artist_tag,artist_class,album_tag,album_class) VALUES ('pitchfork','https://pitchfork.com/reviews/albums/?page=2','div','review','ul','artist-list review__title-artist','h2','review__title-album');
+INSERT INTO source (title,url,container_tag,container_class,artist_tag,artist_class,album_tag,album_class) VALUES ('pitchfork','https://pitchfork.com/reviews/albums/?page=3','div','review','ul','artist-list review__title-artist','h2','review__title-album');
+INSERT INTO source (title,url,container_tag,container_class,artist_tag,artist_class,album_tag,album_class) VALUES ('pitchfork','https://pitchfork.com/reviews/albums/?page=4','div','review','ul','artist-list review__title-artist','h2','review__title-album');
+INSERT INTO source (title,url,container_tag,container_class,artist_tag,artist_class,album_tag,album_class) VALUES ('pitchfork','https://pitchfork.com/reviews/albums/?page=5','div','review','ul','artist-list review__title-artist','h2','review__title-album');
+INSERT INTO source (title,url,container_tag,container_class,artist_tag,artist_class,album_tag,album_class) VALUES ('pitchfork','https://pitchfork.com/reviews/albums/?page=6','div','review','ul','artist-list review__title-artist','h2','review__title-album');
+INSERT INTO source (title,url,container_tag,container_class,artist_tag,artist_class,album_tag,album_class) VALUES ('pitchfork','https://pitchfork.com/reviews/albums/?page=7','div','review','ul','artist-list review__title-artist','h2','review__title-album');
+INSERT INTO source (title,url,container_tag,container_class,artist_tag,artist_class,album_tag,album_class) VALUES ('pitchfork','https://pitchfork.com/reviews/albums/?page=8','div','review','ul','artist-list review__title-artist','h2','review__title-album');
+INSERT INTO source (title,url,container_tag,container_class,artist_tag,artist_class,album_tag,album_class) VALUES ('pitchfork','https://pitchfork.com/reviews/albums/?page=8','div','review','ul','artist-list review__title-artist','h2','review__title-album');
+INSERT INTO source (title,url,container_tag,container_class,artist_tag,artist_class,album_tag,album_class) VALUES ('pitchfork','https://pitchfork.com/reviews/albums/?page=10','div','review','ul','artist-list review__title-artist','h2','review__title-album');
 
 insert into api (name) values ("discogs");
