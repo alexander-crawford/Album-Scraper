fa88356ed3f9fd2b5053d34a64ab24399da12d25
Alexander Crawford <mail@alexander-crawford.com>
2022-01-12 21:27:47 +0100

    sanitise and validate http variables in print_album

--- print_album.php
+++ print_album.php
@@ -17,13 +17,35 @@
         <span class="artist_id" hidden><?php echo $row['artist_id'] ?></span>
         <span class="album_id" hidden><?php echo $row['album_id'] ?></span>
 
-        <?php if (empty($_GET['position'])): ?>
+        <?php
+
+          if (isset($_GET['position'])) {
+
+            $pos_raw = $_GET['position'];
+
+            $pos_sanitised = filter_var($pos_raw,FILTER_SANITIZE_NUMBER_INT);
+
+            if (filter_var($pos_sanitised,FILTER_VALIDATE_INT)) {
+
+              $position = $pos_sanitised;
+
+            }
+
+          }else {
+
+            $position = -1;
+
+          }
+
+        ?>
+
+        <?php if ($position == -1): ?>
 
         <p class="position"><?php echo $row['position'] ?></p>
 
         <?php else: ?>
 
-        <p class="position" hidden><?php echo $_GET['position'] ?></p>
+        <p class="position" hidden><?php echo $position ?></p>
 
         <?php endif; ?>
 
